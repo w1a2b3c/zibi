df6ce08fe5a63d62a4cbdb3b2b76cedc8fe3091c
魏涛 <127228647+w1a2b3c@users.noreply.github.com>
2024-11-06 21:46:42 +0800

    Add files via upload

---
+++ pule/js/htpublic.js
@@ -0,0 +1,91 @@
+(function() {
+    if (typeof WeixinJSBridge == "object" && typeof WeixinJSBridge.invoke == "function") {
+        handleFontSize();
+    } else {
+        if (document.addEventListener) {
+            document.addEventListener("WeixinJSBridgeReady", handleFontSize, false);
+        } else if (document.attachEvent) {
+            document.attachEvent("WeixinJSBridgeReady", handleFontSize);
+            document.attachEvent("onWeixinJSBridgeReady", handleFontSize);
+        }
+    }
+    function handleFontSize() {
+        WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+        WeixinJSBridge.on('menu:setfont', function() {
+            WeixinJSBridge.invoke('setFontSizeCallback', { 'fontSize' : 0 });
+        });
+    }
+})();
+var docSize;
+(function(doc, win) {
+	var docEl = doc.documentElement,
+	isIOS = navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+	dpr = isIOS ? Math.min(win.devicePixelRatio, 3) : 1,
+	dpr = window.top === window.self ? dpr : 1, //被iframe引用时，禁止缩放
+	dpr = 1,
+	scale = 1 / dpr,
+	resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+	docEl.dataset.dpr = dpr;
+	var metaEl = doc.createElement('meta');
+	metaEl.name = 'viewport';
+	metaEl.content = 'initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale;
+	docEl.firstElementChild.appendChild(metaEl);
+	var recalc = function() {
+		var width = docEl.clientWidth;
+		if (width / dpr > 750) {
+			width = 750 * dpr;
+		}
+		// 乘以100，px : rem = 100 : 1
+		docEl.style.fontSize = 100 * (width / 750) + 'px';
+		docSize = 100 * (width / 750);
+	};
+	recalc()
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+})(document, window);
+
+function httipsFun(tex,type){
+	if($('.httips').length>0){
+		return false
+	}
+	$('body').append('<div class="httips"><span>'+tex+'</span></div><div class="httipsbg"></div>');
+	$('.httipsbg').height($('body,html').height())
+	if(type != true){
+		setTimeout(function(){
+			$('.httips').remove();
+			$('.httipsbg').remove()
+		},1000)
+	};
+	$('.httipsbg').on('touchmove', function(e){
+		e.preventDefault();
+		e.stopPropagation();
+	});
+}
+
+function httipsRemove(){
+	$('.httips').remove();
+	$('.httipsbg').remove();
+}
+function httipsbgFun(){
+	$('body').append('<div class="httipsbgajax"></div>');
+	$('.httipsbgajax').height($('body,html').height())
+	$('.httipsbgajax').on('touchmove',function(e){
+		e.preventDefault();
+		e.stopPropagation();
+	});
+}
+function httipsbgRemove(){
+	$('.httipsbgajax:last').remove();
+}
+
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if(typeof window.onload != 'function'){
+		window.onload= func;
+	}else{
+		window.onload =function (){
+			oldonload();
+			func();
+		}
+	}
+}
